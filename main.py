f4fe7debb7aa73cd80d614c8b641f692b6efb482
quockhanhhoangqkh <quockhanhhoangqkh@gmail.com>
2025-07-17 10:29:06 +0700

    main.py

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from typing import List
 import pandas as pd
 from prophet import Prophet
 
-class Point(BaseModel):
+app = FastAPI()
+
+class DataPoint(BaseModel):
     ds: str
     y: float
 
-app = FastAPI()
-
 @app.post("/forecast")
-def forecast(data: List[Point]):
+def forecast(data: List[DataPoint]):
     df = pd.DataFrame([d.dict() for d in data])
     model = Prophet()
     model.fit(df)
     future = model.make_future_dataframe(periods=30)
-    fc = model.predict(future)
-    return fc[['ds','yhat']].tail(30).to_dict(orient="records")
+    forecast = model.predict(future)
+    return forecast[['ds', 'yhat']].tail(30).to_dict(orient="records")
